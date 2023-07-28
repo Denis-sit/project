1dbd78ea04beae98389514c497143f37992f0c76
Denis <specden990@mail.ru>
2023-07-28 21:25:06 +0500

    test

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+// let id = Symbol("id");
+// alert(id.description);
+
+// let a = confirm("Ты тут?");
+// console.log(a);
+
+
+
+
+
+// const answers = [];
+
+// answers[0] = prompt("Как васзовут?", "");
+
+// answers[1] = prompt("Как ваша фамилия?", "");
+
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// // document.write(answers);
+// let n = document.querySelector(".name");
+// let a = document.querySelector(".surname");
+// let b = document.querySelector(".age");
+// console.log(document.querySelector('.name'));
+
+
+// n.textContent = answers[0];
+// a.textContent = answers[1];
+// b.textContent = answers[2];
+
+
+const userName = "Denis";
+
+alert(`Hello,${userName}`);
